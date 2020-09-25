a5ce3f2c31457323d0225db45cd5374f0b015198
Thalyta Fabrine <thalyta.trindade@vtex.com.br>
2020-09-25 11:16:39 -0300

    Remove unused function

--- react/utils/dom-utils.ts
+++ react/utils/dom-utils.ts
@@ -4,20 +4,3 @@ export function getCookie(name: string): string | null {
 
   return match ? decodeURIComponent(match[2]) : null
 }
-
-export function setCookie(
-  key: string,
-  value: string,
-  ttl?: number,
-  path: string = '/'
-) {
-  let expires = ''
-
-  if (ttl && ttl > 0) {
-    const expirationDate = new Date()
-    expirationDate.setTime(new Date().getTime() + ttl)
-    expires = `expires=${expirationDate.toUTCString()};`
-  }
-
-  document.cookie = `${key}=${encodeURIComponent(value)};${expires}path=${path}`
-}
