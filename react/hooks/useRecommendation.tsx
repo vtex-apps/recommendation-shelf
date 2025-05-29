7828bfe2a39bd484e5d23c7c3e46e61c4089e1f8
Felipe Sales <sales.felipeb@gmail.com>
2025-05-29 10:37:55 -0300

    Fix campaignType on fetch recommendations

--- react/hooks/useRecommendation.tsx
+++ react/hooks/useRecommendation.tsx
@@ -1,37 +1,66 @@
+/* eslint-disable padding-line-between-statements */
 import { useQuery } from 'react-apollo'
 import { canUseDOM } from 'vtex.render-runtime'
 
 import type { Args, Response } from '../graphql/QueryRecommendationShelf.gql'
 import recommendationQuery from '../graphql/QueryRecommendationShelf.gql'
 
-function useRecommendation({
-  recommendationType,
-  campaignVrn,
-  products,
-  userId,
-}: {
-  recommendationType: RecommendationType
+type RecommendationInput = {
+  campaignType: RecommendationType
   campaignVrn?: string
   products: string[]
-  userId: string
-}) {
-  const variables: Args = campaignVrn
-    ? {
-        userId,
-        campaignVrn,
-        products,
-      }
+  userId?: string
+}
+
+function getRecommendationArguments(input: RecommendationInput): Args | null {
+  const { campaignType, userId, products } = input
+
+  let args: Args = input.campaignVrn
+    ? ({
+        campaignVrn: input.campaignVrn,
+      } as Args)
     : {
-        userId,
-        campaignType: recommendationType,
-        products,
+        campaignType: input.campaignType,
       }
+
+  switch (campaignType) {
+    case 'CROSS_SELL':
+    case 'SIMILAR_ITEMS':
+      if (products.length === 0) {
+        return null
+      }
+      args = {
+        ...args,
+        products: [products[0]],
+      }
+
+      break
+    case 'LAST_SEEN':
+    case 'PERSONALIZED':
+      if (!userId) {
+        return null
+      }
+      args = {
+        ...args,
+        userId,
+      }
+      break
+    default:
+      args = {}
+      break
+  }
+
+  return args
+}
+
+function useRecommendation(args: RecommendationInput) {
+  const variables = getRecommendationArguments(args)
 
   const { error, data, loading } = useQuery<Response, Args>(
     recommendationQuery,
     {
-      variables,
-      skip: !canUseDOM || !campaignVrn || !userId,
+      variables: variables as Args,
+      skip: !canUseDOM || !variables,
       notifyOnNetworkStatusChange: true,
     }
   )
